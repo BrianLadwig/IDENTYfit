0d18726f7ffcb3423e05370b74f16ab192e2eab5
Brian Ladwig <br.ladwig@gmail.com>
2022-01-31 22:37:37 +0100

    responsiv

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById('burger');
+
+burger.addEventListener('click', () => {
+
+    const burgerMenu = document.getElementById('burger--menu');
+
+        burgerMenu.classList.toggle('show')
+
+});
